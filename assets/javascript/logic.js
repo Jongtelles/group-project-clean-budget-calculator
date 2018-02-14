62950dd747ccc1ce767ef21464dab388cae3306c
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-02-14 14:12:05 -0500

    base functionality for input bar

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,22 @@
-console.log("Testing")
+var recordCategory = function(){
+    var userCategory = $("#userInput").val();
+    console.log("category: " + userCategory);
+    localStorage.setItem("category", userCategory);
+    console.log(incomeSubmitted);
+
+}
+
+var recordIncome = function() {
+    var userIncome = $("#userInput").val();
+    console.log("income: " + userIncome);
+    localStorage.setItem("income", userIncome);
+    incomeSubmitted = true;
+}
+$("#submit").on("click", function(event) {
+ if(incomeSubmitted == false) {
+    recordIncome();
+    }
+else{
+    recordCategory()
+}
+});
