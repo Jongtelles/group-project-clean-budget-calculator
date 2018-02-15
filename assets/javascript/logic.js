cc9fdd6598529f6a07b090c4ba7e66e1090c4f47
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-02-15 10:44:35 -0500

    replaced Travel with Savings

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -25,8 +25,8 @@ $(document).ready(function () {
                 isTracked: false,
                 percentage: 0
             },
-            catTravel: {
-                name: "Travel",
+            catSavings: {
+                name: "Savings",
                 isTracked: false,
                 percentage: 0
             },
@@ -78,9 +78,9 @@ $(document).ready(function () {
             budgetInfo.categories.catEntertainment.isTracked = true;
             console.log("catEntertainment is being tracked " + budgetInfo.categories.catEntertainment.isTracked);
         }
-        if($(this).attr("id") === "travel"){
-            budgetInfo.categories.catTravel.isTracked = true;
-            console.log("catTravel is being tracked " + budgetInfo.categories.catTravel.isTracked);
+        if($(this).attr("id") === "savings"){
+            budgetInfo.categories.catSavings.isTracked = true;
+            console.log("catSavings is being tracked " + budgetInfo.categories.catSavings.isTracked);
         }
         if($(this).attr("id") === "transportation"){
             budgetInfo.categories.catTransportation.isTracked = true;
