e32d38a8e6e312901352bb970cda274b9ad54351
jongtelles <jongtelles@gmail.com>
2018-02-15 10:18:18 -0500

    Updated dropdown menu with jQuery

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -58,29 +58,28 @@ $(document).ready(function () {
     // this is my solution to the category picker. In the final version it will involve replacing the input section with a dropdown or scroll menu, like the one in the password generator, that includes our bategories.
     //it will likely make the funciton i wrote to take user input for categoires unecessary
     $(".dropdown-item").on("click", function(event){
-
-        if(this.innerHTML.includes("Food") == true){
+        if($(this).attr("id") === "food"){
             // $("#food").addClass("is-active");
             budgetInfo.categories.catFood.isTracked = true;
             console.log("catFood is being tracked " + budgetInfo.categories.catFood.isTracked);
         };
-        if(this.innerHTML.includes("Clothing") == true){
+        if($(this).attr("id") === "clothing"){
             budgetInfo.categories.catClothing.isTracked = true;
             console.log("catClothing is being tracked " + budgetInfo.categories.catClothing.isTracked);
-        };        
-        if(this.innerHTML.includes("Entertainment") == true){
+        };
+        if($(this).attr("id") === "entertainment"){
             budgetInfo.categories.catEntertainment.isTracked = true;
             console.log("catEntertainment is being tracked " + budgetInfo.categories.catEntertainment.isTracked);
         }
-        if(this.innerHTML.includes("Travel") == true){
+        if($(this).attr("id") === "travel"){
             budgetInfo.categories.catTravel.isTracked = true;
             console.log("catTravel is being tracked " + budgetInfo.categories.catTravel.isTracked);
         }
-        if(this.innerHTML.includes("Transportation") == true){
+        if($(this).attr("id") === "transportation"){
             budgetInfo.categories.catTransportation.isTracked = true;
             console.log("catTransportation is being tracked " + budgetInfo.categories.catTransportation.isTracked);
         }
-        if(this.innerHTML.includes("Other") == true){
+        if($(this).attr("id") === "other"){
             budgetInfo.categories.catOther.isTracked = true;
             console.log("catOther is being tracked " + budgetInfo.categories.catOther.isTracked);
         }
@@ -90,8 +89,8 @@ $(document).ready(function () {
     addBudgetItem(budgetInfo.categories.catFood.name, 100);
     addBudgetItem("Other", 500);
     console.log(budgetInfo.budgetItems[0], budgetInfo.budgetItems[1]);
-    
-    // records user input as category 
+
+    // records user input as category
     var recordCategory = function(){
         var userCategory = $("#userInput").val();
         console.log("category: " + userCategory);
