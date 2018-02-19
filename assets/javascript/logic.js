1dda1aba0f0e142ebe21594d851349151b931921
unknown <marcushilaire@gmail.com>
2018-02-19 15:16:09 -0500

    increased alpha on colors

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -73,12 +73,12 @@ $(document).ready(function () {
              // percent allocations will be pushed to the array below
              data: [],
              backgroundColor: [
-                 'rgba(255, 99, 132, 0.2)',
-                 'rgba(54, 162, 235, 0.2)',
-                 'rgba(255, 206, 86, 0.2)',
-                 'rgba(75, 192, 192, 0.2)',
-                 'rgba(153, 102, 255, 0.2)',
-                 'rgba(255, 159, 64, 0.2)',
+                 'rgba(255, 99, 132, 0.6)',
+                 'rgba(54, 162, 235, 0.6)',
+                 'rgba(255, 206, 86, 0.6)',
+                 'rgba(75, 192, 192, 0.6)',
+                 'rgba(153, 102, 255, 0.6)',
+                 'rgba(255, 159, 64, 0.6)',
              ],
              borderWidth: 1,
          }],
@@ -93,8 +93,6 @@ $(document).ready(function () {
          myChart.data.datasets[0].data.push(allocation);
          myChart.data.labels.push(tooltip);
          myChart.update();
-         console.log(budgetInfo.spendingMoney+ " - Spending money");
-         console.log(budgetInfo.categories.catFood.percentage+ " - food % allocation");
          console.log(myChart.data.datasets[0].data+ " - data pushed to array");
          console.log( myChart.data.labels+ " - label pushed to array");
      };
